ae4601ba578be6291823e57afa28236d9390431b
Mateo Alarcon <mateoalarconpons@gmail.com>
2026-05-20 11:12:45 -0300

    fix(cors): add all standard headers + x-citadoc-public-key to Access-Control-Allow-Headers

--- supabase/functions/send-email/index.ts
+++ supabase/functions/send-email/index.ts
@@ -5,7 +5,7 @@ const FROM = 'CitaDoc <hola@citadoc.lat>'
 
 const CORS = {
   'Access-Control-Allow-Origin': '*',
-  'Access-Control-Allow-Headers': 'content-type, x-citadoc-public-key',
+  'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type, x-citadoc-public-key',
   'Content-Type': 'application/json',
 }
 
